84f02e58b05bc41f7d5ef9b4a1e006ba504b5a1b
xelser <dkzenzuri@gmail.com>
2023-01-17 20:10:12 +0800

    Disable installing cursor themes

--- install.sh
+++ install.sh
@@ -95,7 +95,7 @@ install_theme() {
   ln -sr "${THEME_DIR}/24"                                                       "${THEME_DIR}/24@2x"
   ln -sr "${THEME_DIR}/scalable"                                                 "${THEME_DIR}/scalable@2x"
 
-  cp -r "${SRC_DIR}/src/cursors/dist${brightprefix}"                             "${THEME_DIR}/cursors"
+  #cp -r "${SRC_DIR}/src/cursors/dist${brightprefix}"                             "${THEME_DIR}/cursors"
   gtk-update-icon-cache "${THEME_DIR}"
 }
 
