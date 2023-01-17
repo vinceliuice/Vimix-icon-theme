81a5e9fd32dd76247252fa02947311e0acca178f
xelser <dkzenzuri@gmail.com>
2023-01-17 11:51:28 +0800

    Fix

--- install.sh
+++ install.sh
@@ -96,7 +96,7 @@ install_theme() {
   ln -sr "${THEME_DIR}/scalable"                                                 "${THEME_DIR}/scalable@2x"
 
   #cp -r "${SRC_DIR}/src/cursors/dist${brightprefix}"                             "${THEME_DIR}/cursors"
-  #gtk-update-icon-cache "${THEME_DIR}"
+  gtk-update-icon-cache "${THEME_DIR}"
 }
 
 while [ $# -gt 0 ]; do
